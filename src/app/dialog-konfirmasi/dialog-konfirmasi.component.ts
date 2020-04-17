83215df92bc65af46cd6c4640a68b0a92a353eb4
ukelillahita <uke1800016153@webmail.uad.ac.id>
2020-04-17 17:09:49 +0700

    praktikum 4

---
+++ src/app/dialog-konfirmasi/dialog-konfirmasi.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-dialog-konfirmasi',
+  templateUrl: './dialog-konfirmasi.component.html',
+  styleUrls: ['./dialog-konfirmasi.component.css'],
+})
+export class DialogKonfirmasiComponent implements OnInit {
+  constructor(
+    public api: ApiService,
+    public dialogRef: MatDialogRef<DialogKonfirmasiComponent>,
+    @Inject(MAT_DIALOG_DATA) public id: any
+  ) {}
+
+  ngOnInit(): void {}
+  konfirmasi() {
+    this.dialogRef.close(true);
+  }
+}
